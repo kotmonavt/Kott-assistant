abdc8787079062de3e130e5dcc65e16a7af957c1
kotmonavt <kridtina126zz@gmail.com>
2022-06-27 21:52:17 +0300

    search, exit, div, error done

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import sys
 import webbrowser
-
 import speech_recognition as sr
-
-# Блок команд
-command = {
-    "search": ('найди', 'поиск', 'найти'),
-    "open_manager": ('диспетчер задач'),
-    "open_settings": ('настройки', 'панель управления'),
-    "close": ('выход', 'завершение', 'закрыть', 'выйди', 'закрой'),
-    "open_folder": ('папку'),
-    "open": ('открой', 'открыть'),
-    "wifi_on": ('включи', 'включить'),
-    "wifi_off": ('выключи', 'выключить'),
-    "div": ('раздели', "разделить", 'делить')
-}
 
 
 # Модуль для распознавания речи
@@ -35,15 +21,25 @@ def take_command():
 # Модуль выполнения
 def execute():
     command = (take_command().split(" "))
-    if command[0] == 'поиск' or 'найди' or 'найти':
+    if command[0] == 'поиск' or command[0] == 'найди' or command[0] == 'найти':
         command.pop(0)
         search_name = " ".join(command)
         url = "https://www.google.com/search?q=" + search_name
         webbrowser.get().open(url)
-    elif command[0] == 'выход' or 'выйди' or 'завершение':
+    elif command[0] == 'выход' or command[0] == 'выйди' or command[0] == 'завершение':
         sys.exit()
+    elif command[0] == 'раздели' or command[0] == 'разделить':
+        divisible = int(command[1])
+        divider = int(command[3])
+        if divider != 0:
+            print("Результат деления", divisible, "на", divider, "равен", divisible/divider )
+        else:
+            print("Правила математики запрещают мне делить на ноль, о мешок с костями")
+    elif 'открой диспетчер задач' in take_command() or 'открыть диспетчер задач' in take_command():
+        pass
+    else:
+        print("Команда не распознана, попробуйте снова")
+
 
 while True:
     execute()
-
-
